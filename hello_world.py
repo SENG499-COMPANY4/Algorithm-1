35f285128e53f7f833900053713dab205538a3c4
JesseLacroix <jesselacroix394@gmail.com>
2023-05-27 15:45:17 -0700

    Added wsgi and made changes to hello_world

--- hello_world.py
+++ hello_world.py
@@ -1,11 +1,7 @@
 #hello_world.py
 from flask import Flask
-from application import app
 
-if __name__ == '__main__':
-    app.run(debug=False)
-
-#app = Flask(__name__)
+app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
