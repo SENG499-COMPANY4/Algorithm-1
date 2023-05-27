02f825017407c7b3fe8e2515d6a67ac86d4032ae
JesseLacroix <jesselacroix394@gmail.com>
2023-05-27 15:42:52 -0700

    Added setup.py and updated hello_world

--- hello_world.py
+++ hello_world.py
@@ -1,8 +1,11 @@
 #hello_world.py
-
 from flask import Flask
+from application import app
 
-app = Flask(__name__)
+if __name__ == '__main__':
+    app.run(debug=False)
+
+#app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
