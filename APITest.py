c7ac3f1afc769eaee278c13bc41738dcf0a71be2
lsancer99 <80100275+DeweiYu@users.noreply.github.com>
2023-07-21 14:32:06 -0700

    Update APITest.py
    manual merge the APItrial test with API test

--- APITest.py
+++ APITest.py
@@ -63,6 +63,69 @@ def test_APIGetScheduleNoAuth():
     
     assert int(response.status_code) == 401
 
+def test_APIPostValid():
+    # Mock the successful API response for the valid case
+    mock_request_data = {"data": "sample data for POST request"}
+    mock_response_data = {"status": "success"}
+    mock_status_code = 200
+
+    # Start the mocking session
+    with requests_mock.Mocker() as m:
+        # Mock the response for the API endpoint
+        environment = 'Stage'
+        base_address = getAddress(environment)
+        mock_route = '/submitData'
+        m.post(base_address + mock_route, json=mock_response_data, status_code=mock_status_code)
+
+        # Now, call the postRequest function without tokens
+        response = postRequest(base_address, mock_route, data=mock_request_data)
+
+    # Assertions
+    assert int(response.status_code) == mock_status_code
+    assert response.json() == mock_response_data
+
+def test_APIPostInvalid():
+    # Mock the API response for the invalid case
+    mock_request_data = {"data": "invalid data for POST request"}
+    mock_response_data = {"error": "Invalid data"}
+    mock_status_code = 500
+
+    # Start the mocking session
+    with requests_mock.Mocker() as m:
+        # Mock the response for the API endpoint
+        environment = 'Stage'
+        base_address = getAddress(environment)
+        mock_route = '/submitData'
+        m.post(base_address + mock_route, json=mock_response_data, status_code=mock_status_code)
+
+        # Now, call the postRequest function without tokens
+        response = postRequest(base_address, mock_route, data=mock_request_data)
+
+    # Assertions
+    assert int(response.status_code) == mock_status_code
+    assert response.json() == mock_response_data
+
+def test_APIPostNoToken():
+    # Mock the API response when making a POST request without a token
+    mock_request_data = {"data": "sample data for POST request"}
+    mock_response_data = {"error": "Unauthorized"}
+    mock_status_code = 401
+
+    # Start the mocking session
+    with requests_mock.Mocker() as m:
+        # Mock the response for the API endpoint
+        environment = 'Stage'
+        base_address = getAddress(environment)
+        mock_route = '/submitData'
+        m.post(base_address + mock_route, json=mock_response_data, status_code=mock_status_code)
+
+        # Now, call the postRequest function without tokens
+        response = postRequest(base_address, mock_route, data=mock_request_data)
+
+    # Assertions
+    assert int(response.status_code) == mock_status_code
+    assert response.json() == mock_response_data
+
 def test_APIPutScheduleInvalid():
 
     environment = getAddress('Stage')
@@ -189,6 +252,9 @@ def test_APIValidateNoAuth():
 if __name__ == "__main__":
     test_APIGetScheduleValid()
     test_APIGetScheduleNoAuth()
+    test_APIPostValid()
+    test_APIPostInvalid()
+    test_APIPostNoToken()
     test_APIPutScheduleInvalid()
     test_APIPutScheduleNoAuth()
     test_APIPutScheduleValid()
